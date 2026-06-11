20b032bc40c8c113af4fbebb3a1b6ec3b6a3b93b
risaphnmama <risa1004risa@gmail.com>
2026-06-12 01:13:20 +0900

    新Supabaseプロジェクトに接続先を更新

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
-﻿import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://gjrinxrbjrcriujbgoia.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdqcmlueHJianJjcml1amJnb2lhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2ODgyMTUsImV4cCI6MjA5NjI2NDIxNX0.QjEP_N_mPU4KQvG_wGrvvLG_yfhvbUeMV68NB2Ng75k';
+const SUPABASE_URL = 'https://geqggqlfaqwpcazbjlxg.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_0-oeDbPpCWFX7RICB6mpGA_YR4gChOb';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
